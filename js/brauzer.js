c15302db5a812de487bf7d42b3c25249cd304df6
NetTim33 <timur.krasavchik.91@mail.ru>
2021-09-17 22:51:18 +0500

    project po js

---
+++ js/brauzer.js
@@ -0,0 +1,25 @@
+"use strict";
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = prompt("Вам есть 18?", "18");
+// console.log(typeof(answer));
+
+// const answer = +prompt("Вам есть 18?", "18");
+// console.log(answer + 5);  
+
+const answers = [];
+
+answers[0] = prompt('Как ваше имя', '');
+answers[1] = prompt('Как ваше фамилия', '');
+answers[2] = prompt('Сколько вам лет', '');
+
+document.write(answers);
+
+//////////////////////////////////////
+
+// const user = "Timur";
+// alert(`Привет, ${user}`);
+
+//////////////////////////////
+
