7c6eb41c583a320a0fd4c6efe3e352b65c2e70a9
NetTim33 <timur.krasavchik.91@mail.ru>
2021-09-21 22:38:10 +0500

    urok po jsV7

--- js/javascript.js
+++ js/javascript.js
@@ -395,12 +395,81 @@ console.log(parseFloat(test));            //метод возврашает из
 
 ////////////////////////////////////////////////////////////////////////
 
+function first() {
 
+    setTimeout(function() {
+        console.log(1);
+    }, 500);
+}
 
+function second() {
+    console.log(2);
+}
 
+first();
+second();
 
+///////////////////////////////////////////////////////////////////////
 
+function learnJS (lang, callback) {
+    console.log(`Я учу: ${lang}`);
+    callback();
+}                                                              //callback
 
+learnJS('JavaScript', function() {
+    console.log('Я прошел этот урок!');
+});
 
+///////////////////////////////////////////////////////////////////////
 
+function learnJS (lang, callback) {
+    console.log(`Я учу: ${lang}`);
+    callback();
+}                                                             //callback               
+
+function done() {
+    console.log('Я прошел этот урок!');
+}
+
+learnJS('JavaScript', done);
+
+///////////////////////////////////////////////////////////////////////
+
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    },
+    makeTest: function() {
+        console.log('Test');
+    }
+};
+
+options.makeTest();
+const {border, bg} =options.colors;
+console.log(border);
+
+console.log(Object.keys(options).length);                        //получить масив со всеми ключами  
+// console.log(options.name);
+// delete options.name;
+// console.log(options);
+
+let counter = 0;
+for (let key in options) {
+    if (typeof(options[key]) === 'object'){
+        for (let i in options[key]) {
+            console.log(`Свойства ${i} имеет значения ${options[key][i]}`);
+            counter++;
+        }
+    } else{
+        console.log(`Свойства ${key} имеет значения ${options[key]}`);
+        counter++;
+    }
+}
+console.log(counter);
+
+///////////////////////////////////////////////////////////////////////
 
