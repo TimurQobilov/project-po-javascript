59265cccf1b57c174c20e02152f4fc6deb193ba5
NetTim33 <timur.krasavchik.91@mail.ru>
2021-09-29 22:38:27 +0500

    Урок на сатйте V1

--- Урок js работа на сайте/js/script.js
+++ Урок js работа на сайте/js/script.js
@@ -24,3 +24,33 @@ const movieDB = {
     ]
 };
 
+const adv = document.querySelectorAll('.promo__adv img'),
+      poster = document.querySelector('.promo__bg'),
+      ganre = poster.querySelector('.promo__genre'),
+      moviList = document.querySelector('.promo__interactive-list');
+
+adv.forEach(item => {
+    item.remove();            //удалил рекламу через функцию
+});
+
+ganre.textContent = 'драма';                           //изменил жанар 
+poster.style.backgroundImage = 'url("img/bg.jpg")';   //изменил фон сайта 
+
+// moviList.forEach(item => {
+//     item.remove();           //удалил через функцию не нужный текст через селектор querySelectorAll
+// });
+moviList.innerHTML = "";      //очистил текст замениф пустой строкой
+
+movieDB.movies.sort();      //сортировка по алфавиту
+
+// console.log(poster.innerHTML);   //можно получить таким способом код html в консоле браузера
+
+movieDB.movies.forEach((fims, i) => {            //поменял текст на сайте заменив эго 
+    moviList.innerHTML += `
+        <li class="promo__interactive-item">${i + 1} ${fims}
+            <div class="delete"></div>
+        </li>
+    `;
+});
+
+
