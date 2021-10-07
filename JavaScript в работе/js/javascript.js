8ac9a3433e4621020e66b4491ac75fda3d600826
NetTim33 <timur.krasavchik.91@mail.ru>
2021-10-07 10:25:10 +0500

    раздел 3 урок 40

--- JavaScript в работе/js/javascript.js
+++ JavaScript в работе/js/javascript.js
@@ -1,132 +1,28 @@
-// const timerId = setTimeout(function () {                //запустится анимация через 2 секунды
-//     console.log('Timur');                   
-// }, 2000);
-
-// const timerId = setTimeout(function (text) {                //запустится анимация через 2 секунды
-//     console.log(text);                   
-// }, 2000, 'Timur');
-
-// const timerId = setTimeout(logger, 2000);             //запустится анимация через 2 секунды
-// function logger () {
-//     console.log('Timur');
-// }
-
-// setTimeout(logger, 2000);                                    //запустится анимация через 2 секунды
-// function logger () {
-//     console.log('Timur');
-// }
-
-// const timerId = setTimeout(logger, 2000);             //запустится анимация через 2 секунды
-
-// clearInterval(timerId);                               //здесь останавливается анимация 
-// function logger () {
-//     console.log('Timur');
-// }
-
-// const btn = document.querySelector('.btn');
-
-// btn.addEventListener('click', () => {
-//     // const timerId = setTimeout(logger, 2000);             //запустится анимация через 2 секунды после нажатия на кнопку 
-//     const timerId = setInterval(logger, 2000);
-// });
-
-// // clearInterval(timerId);
-
-// function logger () {
-//     console.log('Timur');
-// }
-
-// const btn = document.querySelector('.btn');             
-// let timerId;
-// btn.addEventListener('click', () => {
-//     timerId = setTimeout(logger, 2000);             //запустится анимация через 2 секунды после клика на кнопку 
-// });
-
-// clearInterval(timerId);
-
-// function logger () {
-//     console.log('Timur');
-// }
-
-// const btn = document.querySelector('.btn');             
-// let timerId,
-//     i = 0;
-// btn.addEventListener('click', () => {
-//     timerId = setInterval(logger, 500);             //запустится анимация через 0.5 секунды после клика на кнопку 
-// });
-
-// function logger () {
-//     if (i === 3) {
-//         clearInterval(timerId);
-//     }
-//     console.log('Timur');
-//     i++;
-// }               
-
-// const btn = document.querySelector('.btn');             
-// let timerId,
-//     i = 0;
-// btn.addEventListener('click', () => {
-//     timerId = setInterval(logger, 500);             //запустится анимация через 0.5 секунды после клика на кнопку 
-// });
-
-// function logger () {
-//     if (i === 3) {
-//         clearInterval(timerId);
-//     }
-//     console.log('Timur');
-//     i++;
-// }   
-
-// let id = setTimeout(function log () {
-//     console.log('Hello');
-//     id = setTimeout (log, 500);
-// }, 500); 
+const now = new Date();                           //Получил дату 
+console.log(now);
+// Это методы геторы 
+console.log(now.getFullYear());                   //получил год через метод 
+console.log(now.getMonth());                      //получил месяц через метод 
+console.log(now.getDate());                          //получил дату
+console.log(now.getHours());                         //получил время 
+console.log(now.getUTCHours());                      //получил временую разницу
+console.log(now.getTimezoneOffset());                //разница между часовым поисом 
+console.log(now.getTime());                          //прошедшее время с начала 1970 года
 
 
-// const btn = document.querySelector('.btn');             
-// let timerId,
-//     i = 0;
-// btn.addEventListener('click', () => {
-//     timerId = setInterval(logger, 500);             //запустится анимация через 0.5 секунды после клика на кнопку 
-// });
+//Это методы сеторы 
 
-// function logger () {
-//     if (i === 3) {
-//         clearInterval(timerId);
-//     }
-//     console.log('Timur');
-//     i++;
-// }   
+const sow = new Date();                           //Получил дату 
+console.log(sow.setHours(15)); 
+console.log(sow);
 
-// let id = setTimeout(function log () {
-//     console.log('Hello');
-//     id = setTimeout (log, 500);
-// }, 500); 
+////
 
+let start = new Date();
 
-const btn = document.querySelector('.btn');             
-let timerId,
-    i = 0;
+for (let i = 0; i < 100000; i++) {
+    let some = i ** 3;
+}
+let end =new Date();
 
- 
-function myAnimation() {
-    const elem = document.querySelector('.box');
-    let pos = 0;
-    
-
-    const id = setInterval(frame, 10);
-    function frame() {
-        if (pos == 300) {
-            clearInterval(id);
-        } else {
-            pos++;
-            elem.style.top = pos + "px";
-            elem.style.left = pos + "px";
-        }
-    }
-    
-} 
-btn.addEventListener('click', myAnimation);
-
-
+alert(`Цикл отработал за ${end - start} милисекунд`);
